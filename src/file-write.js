c18fb6b1620a5412d4412032735d839aaf820fc6
BensonChen <472756921@qq.com>
2021-03-31 19:57:08 +0800

    feat: gitlog转 md

--- src/file-write.js
+++ src/file-write.js
@@ -17,5 +17,17 @@ function writeFile(filePath, content) {
 	fs.writeFileSync(filePath, content, "utf8");
 }
 
+function writeFileFormat(filePath, content) {
+	let fileContent = "",
+		index = 0;
+	for (i in content) {
+		if (index) {
+            
+        }
+		index++;
+	}
+}
+
 exports.createIfMissing = createIfMissing;
 exports.writeFile = writeFile;
+exports.writeFileFormat = writeFileFormat;
